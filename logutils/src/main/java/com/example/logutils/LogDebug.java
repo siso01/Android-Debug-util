1c5297fb754696915a9f08e3e704d13ddb99b34a
shailendra <shailendra.sisodiya01@gmail.com>
2019-08-03 14:21:36 +0530

    upate

--- logutils/src/main/java/com/example/logutils/LogDebug.java
+++ logutils/src/main/java/com/example/logutils/LogDebug.java
@@ -3,7 +3,7 @@ package com.example.logutils;
 import android.util.Log;
 
 public class LogDebug {
-    public static String TAG = "SUPER_AWESOME_library";
+    public static String TAG = "SUPER_AWESOME_APP";
     public static void d(String message){
         Log.d(TAG,message);
     }
